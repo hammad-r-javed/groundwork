98560c9d8e628cd2bbd2a9131539003e8a3b8630
hammad-r-javed <128047144+hammad-r-javed@users.noreply.github.com>
2024-12-07 22:44:58 +0000

    remov python req, finish script

---
+++ groundwork.sh
@@ -0,0 +1,18 @@
+currentdir=$(pwd)
+if [ $# -ne 1 ]; then
+    echo "Invalid number of arguments! Please provide a template name!"
+    echo ""
+    echo "Your existing templates are:"
+    ls $GROUNDWORK_TEMPLATES/templates
+    exit
+fi
+
+if [ ! -f "$GROUNDWORK_TEMPLATES/templates/$1/$1.sh" ]; then
+    echo "Invalid template name! Please provide a valid template name!"
+    echo ""
+    echo "Your existing templates are:"
+    ls $GROUNDWORK_TEMPLATES/templates
+    exit
+fi
+
+$GROUNDWORK_TEMPLATES/templates/$1/$1.sh "$currentdir"
